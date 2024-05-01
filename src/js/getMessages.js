8b79f43911edaa199dc9018705bc325050575b84
Nicolas Buntschu <nicolas.buntschu@gmail.com>
2024-05-01 13:53:53 +0200

    Add comments to make it clearer

--- src/js/getMessages.js
+++ src/js/getMessages.js
@@ -9,8 +9,12 @@ import {
 } from 'firebase/firestore'
 import dayjs from 'dayjs'
 
+// Fonction pour formater un message
 const prepareMessage = function (message) {
+  // On formate le temps avec la librairie dayjs (voir https://day.js.org/docs/en/display/format pour les options de formatage)
   const formattedTime = dayjs(message.time.toDate()).format('HH:mm:ss')
+
+  // On retourne le HTML du message
   return `
   <div class="message">
     <div class="message-time">${formattedTime}</div>
@@ -20,8 +24,10 @@ const prepareMessage = function (message) {
 }
 
 const getMessages = function () {
+  // On sélectionne le conteneur des messages
   const messagesContainer = document.querySelector('[data-messages]')
 
+  // On récupère la base de données Firestore
   const db = getFirestore(app)
   const messagesQuery = query(
     collection(db, 'messages'),
@@ -29,7 +35,9 @@ const getMessages = function () {
     limit(20)
   )
 
+  // On écoute les changements sur la collection 'messages'
   onSnapshot(messagesQuery, (querySnapshot) => {
+    // Lorsqu'il y a un changement, on récupère les messages
     const messages = []
     querySnapshot.forEach((doc) => {
       messages.push(doc.data())
@@ -37,10 +45,13 @@ const getMessages = function () {
     messages.reverse()
 
     const messagesHTML = messages.map((message) => {
+      // On formate le message
       return prepareMessage(message)
     })
 
+    // On ajoute les messages dans le conteneur
     messagesContainer.innerHTML = messagesHTML.join('')
+    // On scroll en bas du conteneur
     messagesContainer.scrollTop = messagesContainer.scrollHeight
   })
 }
--- src/js/sendMessage.js
+++ src/js/sendMessage.js
@@ -3,19 +3,27 @@ import { getFirestore, collection, addDoc } from 'firebase/firestore'
 
 const sendMessage = function () {
   const db = getFirestore(app)
+
+  // On sélectionne le formulaire
   const form = document.querySelector('[data-form]')
 
+  // On écoute l'événement submit sur formulaire, pour envoyer un message
   form.addEventListener('submit', function (event) {
+    // On empêche le formulaire de recharger la page
     event.preventDefault()
+
+    // On récupère les champs du formulaire
     const authorField = form.querySelector('[name=author]')
     const textField = form.querySelector('[name=text]')
 
+    // On envoie le message dans la base de données
     addDoc(collection(db, 'messages'), {
       author: authorField.value,
       text: textField.value,
       time: new Date(),
     })
 
+    // On vide le champ de texte
     textField.value = ''
   })
 }
